3028d741a3b4ea4b54418ab32cd70bbf6a090b38
Cesc Colindres <cesccolin5@gmail.com>
2022-10-29 12:36:55 +0200

    versiofinal

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,23 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { HeroeComponent } from './components/heroe/heroe.component';
+import { HeroesComponent } from './components/heroes/heroes.component';
+import { AboutComponent } from './components/about/about.component';
+import { HomeComponent } from './components/home/home.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path:'', component: HeroesComponent},
+  {path:'heroes', component: HeroesComponent},
+  {path:'heroe/:id', component: HeroeComponent},
+  {path: 'about', component:AboutComponent},
+  {path: 'home', component:HomeComponent},
+  {path: '**', component:HomeComponent}
+
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
 })
 export class AppRoutingModule { }
+
